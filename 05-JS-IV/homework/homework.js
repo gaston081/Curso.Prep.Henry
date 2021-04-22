17d977543c3aaaa4288d9962f2063786ea11a403
unknown <gaston081@hotmail.com>
2021-04-22 13:51:32 -0300

    finalizada/05-JS-IV/homework/homework.js

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,14 +6,27 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
-}
 
+var obje = {
+  nombre: nombre,
+  edad: edad,
+  meow: function() { 
+    return "Meow!";
+  }
+};
+return obje;
+}
 
 function agregarPropiedad(objeto, property) {
   // Agrega una propiedad al objeto (argumento "objeto") con el valor `null`
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+objeto[property]= null;
+  return objeto
+
+
+
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -21,13 +34,17 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
-}
 
+objeto[metodo]();
+
+}
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
 
+var total = objetoMisterioso.numeroMisterioso*5;
+return total;
 }
 
 function eliminarPropiedad(objeto, unaPropiedad) {
@@ -35,30 +52,60 @@ function eliminarPropiedad(objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
+delete objeto[unaPropiedad];
+return objeto;
+
+
 }
 
 function nuevoUsuario(nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
+var usuario= {
+ nombre:nombre,
+ email:email,
+ password:password}
+ return usuario;
+
 
 }
+
+
 
 function tieneEmail(usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
-}
 
+  
+
+if (usuario ['email']){
+  return true;
+}
+  else{return false;
+  }
+}
 
 function tienePropiedad(objeto, propiedad) {
   // Devuelve "true" si el objeto tiene el valor del argumento "propiedad"
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+if (objeto[propiedad]){
+  return true;
+}
+else {return false;}
+
 }
 
 function verificarPassword(usuario, password) {
+  if (usuario['password'] === password) {
+    return true;
+    }
+  else return false;
+ 
+
   // Comprueba si la "password" enviada coincide con la propiedad "password" del objeto "usuario"
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
@@ -69,13 +116,19 @@ function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
+usuario.password = nuevaPassword;
+return usuario;
 }
 
-function agregarAmigo(usuario, nuevoAmigo) {
+
+function agregarAmigo(usuario, nuevoamigo) {
   // "usuario" tiene una propiedad llamada "amigos" que es un array
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // // Tu código:
+usuario.amigos.push(nuevoamigo);
+return usuario;
+
 }
 
 function pasarUsuarioAPremium(usuarios) {
@@ -84,6 +137,10 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+  for (i= 0; i < usuarios.length; i++){
+    usuarios[i].esPremium = true;
+  }
+    return usuarios;
 }
 
 function sumarLikesDeUsuario(usuario) {
@@ -91,22 +148,31 @@ function sumarLikesDeUsuario(usuario) {
   // "posts" es un array de objetos "post"
   // Cada objeto "post" tiene una propiedad llamada "likes" que es un entero (int/integer)
   // Suma todos los likes de todos los objetos "post"
-  // Devuelve la suma
-  // Tu código:
+  // Devuelve la suma 
+  //var usuario = { posts: [ {likes:10}]  };
+  var suma = 0;
+
+ for (var i = 0; i < usuario.posts.length; i++){
+  suma = suma + usuario.posts[i].likes}
+ return suma; 
 }
 
-function agregarMetodoCalculoDescuento(producto) {
+
+function agregarMetodoCalculoDescuento(producto, precio) {
   // Agregar un método (función) al objeto "producto" llamado "calcularPrecioDescuento"
   // Este método debe multiplicar el "precio" del "producto" ("producto.precio" o "producto[precio]") y "porcentajeDeDescuento" para obtener el descuento
   // El método resta el descuento del precio y devuelve el precio con descuento
   // Devuelve el objeto "producto" al final de la función
   // Ejemplo:
-  // producto.precio -> 20
+  // producto.precio -> 20npm test JSIV.test.js
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
-  // Tu código:
 
-}
+
+   producto.calcularPrecioDescuento = function() {
+   return producto.precio - ( producto.precio * producto.porcentajeDeDescuento )};
+  return producto;
+  }
 
 // No modificar nada debajo de esta línea
 // --------------------------------
--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+var matriz= [];
+  for (let clave in objeto){
+    matriz.push([clave, objeto[clave]])
+  }
+    return matriz;
 }
 
 
@@ -18,6 +23,16 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+var objeto ={};
+for( i=0;i< string.length; i++){
+  if(objeto[string[i]]){
+    (objeto[string[i]])+=1;
+  }
+    else {
+      (objeto[string[i]]) =1;
+     }
+}
+return objeto;
 }
 
 
@@ -26,7 +41,19 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
-}
+
+  
+var stringmay =  "";
+var stringmin = "";
+    for (i=0; i< s.length; i++){
+      if( s[i].toUpperCase() === s[i] ){
+      stringmay += s[i];}
+       else { stringmin += s[i]; }
+      }
+         return stringmay + stringmin;
+    
+    }
+
 
 
 function asAmirror(str) {
@@ -35,7 +62,15 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
-} 
+ var nuevo = "";
+ 
+  
+ for( var i = str.length-1; i >=0; i--){
+    nuevo = nuevo + str[i];}
+
+var cambio = nuevo.split(" ");
+  return cambio.reverse(). join(" ");
+  } 
 
 
 function capicua(numero){
@@ -43,13 +78,26 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+var string = numero.toString();
+    for(i=0; i<string.length/2; i++){
+      if(string[i] !== string[string.length-1-[i]]){
+        return "No es capicua";
+      }
+    }
+    return "Es capicua"
 }
-
-
 function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var array = '';
+  sacar = ["a", "b", "c"]
+    for (var i=0; i<cadena.length;i ++){
+        if(!sacar.includes(cadena[i])){
+          array = array + cadena[i];
+        }
+    }
+    return array;
 }
 
 
@@ -57,6 +105,9 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+
+  arr.sort(function(a, b){return a.length - b.length});
+  return arr;
 }
 
 
@@ -66,7 +117,17 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
-}
+  var nuevo=[];
+
+      for(var i= 0; i<arreglo1.length; i++ ){
+        if(arreglo2.includes(arreglo1[i])){
+          nuevo.push(arreglo1[i])
+           }
+          }
+      return nuevo;
+          }
+
+
 
 
 
